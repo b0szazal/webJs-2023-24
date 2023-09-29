04d1308ef893e35ed164dcc31665f638c41163ed
b0szazal <zalan.szasz06@gmail.com>
2023-09-29 09:55:08 +0200

    buggy ass js

--- project 1/naptarvagyorarend.js
+++ project 1/naptarvagyorarend.js
@@ -1,22 +1,25 @@
-
+let header=document.getElementById("header");
+let lightdarkmodes=document.getElementsByClassName("light");
 let moonimg=document.getElementById("lighttodark");
 let sunimg=document.getElementById("darktolight")
 
 function DarkMode(){
-    let lightdarkmodes=document.getElementsByClassName("dark");
+    header.classList.remove("light");
+    header.classList.add("dark");
     for(let i=0; i<lightdarkmodes.length; i++){
-        lightdarkmodes[i].classList.remove("dark");
         lightdarkmodes[i].classList.add("light");
+        lightdarkmodes[i].classList.remove("dark");
     }
     moonimg.classList.add("gone");
     sunimg.classList.remove("gone");
 }
 
 function LightMode(){
-    let lightdarkmodes=document.getElementsByClassName("light");
+    header.classList.remove("dark");
+    header.classList.add("light");
     for(let i=0; i<lightdarkmodes.length; i++){
-        lightdarkmodes[i].classList.remove("light");
         lightdarkmodes[i].classList.add("dark");
+        lightdarkmodes[i].classList.remove("light");
     }
     moonimg.classList.remove("gone");
     sunimg.classList.add("gone");
