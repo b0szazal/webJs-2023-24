648a172ce3296d419ec126c3e3bb7da1e28827d8
b0szazal <zalan.szasz06@gmail.com>
2023-09-29 09:40:26 +0200

    buggy push

--- project 1/naptarvagyorarend.js
+++ project 1/naptarvagyorarend.js
@@ -1,18 +1,23 @@
-let header=document.getElementById("header");
+
 let moonimg=document.getElementById("lighttodark");
 let sunimg=document.getElementById("darktolight")
 
 function DarkMode(){
-    header.classList.remove("light");
-    header.classList.add("dark");
+    let lightdarkmodes=document.getElementsByClassName("dark");
+    for(let i=0; i<lightdarkmodes.length; i++){
+        lightdarkmodes[i].classList.remove("dark");
+        lightdarkmodes[i].classList.add("light");
+    }
     moonimg.classList.add("gone");
     sunimg.classList.remove("gone");
 }
 
 function LightMode(){
-    
-    header.classList.remove("dark");
-    header.classList.add("light");
+    let lightdarkmodes=document.getElementsByClassName("light");
+    for(let i=0; i<lightdarkmodes.length; i++){
+        lightdarkmodes[i].classList.remove("light");
+        lightdarkmodes[i].classList.add("dark");
+    }
     moonimg.classList.remove("gone");
     sunimg.classList.add("gone");
 }
