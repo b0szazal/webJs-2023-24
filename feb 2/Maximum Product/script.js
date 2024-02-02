9eb2b0fcc81e84585b61c4fffe100ddc6363509b
b0szazal <zalan.szasz06@gmail.com>
2024-02-02 09:36:08 +0100

    maximum product

---
+++ feb 2/Maximum Product/script.js
@@ -0,0 +1,9 @@
+function adjacentElementsProduct(array) {
+  let highest=-Infinity;
+  for(let i=0; i<array.length-1; i++){
+    if((array[i]*array[i+1])>highest){
+      highest=array[i]*array[i+1];
+    }
+  }
+  return highest
+}
